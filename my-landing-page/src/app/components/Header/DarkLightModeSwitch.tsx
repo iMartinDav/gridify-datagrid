68c5fa21bfdd8035d3cf472112e9b68c317a0522
Martin DAVILA <aldntmi@gmail.com>
2024-02-09 17:01:37 +0100

    toggle almost done just in bigger screen

--- my-landing-page/src/app/components/Header/DarkLightModeSwitch.tsx
+++ my-landing-page/src/app/components/Header/DarkLightModeSwitch.tsx
@@ -1,11 +1,10 @@
 // components/DarkLightModeSwitch.tsx
 
 // Mark this file as a client component
-"use client"
+"use client";
 
 // Import necessary libraries
 import React, { useState, useEffect, createContext, useContext } from "react";
-import { Switch } from "@headlessui/react";
 
 // Custom hook to manage dark mode state and effects
 const useDarkMode = () => {
@@ -29,7 +28,9 @@ const useDarkMode = () => {
       setIsDarkMode(JSON.parse(savedDarkMode));
     } else {
       // If no preference found, set dark mode based on user's OS preference
-      const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      const prefersDarkMode = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
       setIsDarkMode(prefersDarkMode);
     }
   }, []);
@@ -37,9 +38,9 @@ const useDarkMode = () => {
   // Set the data-theme attribute on the html element based on the user's preference
   useEffect(() => {
     if (isDarkMode) {
-      document.documentElement.setAttribute('data-theme', 'dark');
+      document.documentElement.setAttribute("data-theme", "dark");
     } else {
-      document.documentElement.removeAttribute('data-theme');
+      document.documentElement.removeAttribute("data-theme");
     }
   }, [isDarkMode]);
 
@@ -50,7 +51,9 @@ const useDarkMode = () => {
 const DarkModeContext = createContext(false);
 
 // Provider component to wrap the app with the context
-export const DarkModeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const DarkModeProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [isDarkMode, toggleDarkMode] = useDarkMode();
   return (
     <DarkModeContext.Provider value={isDarkMode}>
@@ -69,22 +72,48 @@ const DarkLightModeSwitch: React.FC = () => {
   const [isDarkMode, toggleDarkMode] = useDarkMode();
 
   return (
-    <div className="dark-light-mode-switch">
-      {/* Switch component from Tailwind UI */}
-      <Switch
-        checked={isDarkMode}
-        onChange={toggleDarkMode}
-        className={`${
-          isDarkMode ? "bg-blue-600" : "bg-gray-200"
-        } relative inline-flex items-center h-6 rounded-full w-11`}
-      >
-        <span className="sr-only">Enable dark mode</span>
-        <span
-          className={`${
-            isDarkMode ? "translate-x-6" : "translate-x-1"
-          } inline-block w-4 h-4 transform bg-white rounded-full`}
-        />
-      </Switch>
+    <div className="flex justify-end px-2 sm:px-3 md:px-4 lg:px-5 xl:px-6 2xl:px-8 py-1 sm:py-2 md:py-3 lg:py-4 xl:py-5 2xl:py-6">
+      <div className="dark-light-mode-switch">
+        <button
+          aria-label={
+            isDarkMode ? "Switch to light mode" : "Switch to dark mode"
+          }
+          role="switch"
+          aria-checked={isDarkMode}
+          className={`w-8 sm:w-10 md:w-12 lg:w-13 xl:w-14 2xl:w-14 h-5 sm:h-6 md:h-7 lg:h-7 xl:h-8 2xl:h-8 rounded-full flex items-center transition duration-300 focus:outline-none shadow-lg hover:bg-gray-200 active:bg-gray-300 ${
+            isDarkMode ? "bg-indigo-950" : "bg-white"
+          }`}
+          onClick={toggleDarkMode}
+        >
+          <div
+            id="switch-toggle"
+            className={`w-5 sm:w-6 md:w-7 lg:w-8 xl:w-9 2xl:w-9 h-5 sm:h-6 md:h-7 lg:h-7 xl:h-8 2xl:h-8 relative rounded-full transition duration-500 transform p-1 text-yellow-200 shadow-lg ${
+              isDarkMode
+                ? "bg-violet-950 translate-x-full"
+                : "bg-violet-300 -translate-x-2"
+            }`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="1 2 26 26"
+              stroke="currentColor"
+              className="w-4 sm:w-5 md:w-6 lg:w-7 xl:w-8 2xl:w-8 h-4 sm:h-5 md:h-6 lg:h-7 xl:h-8 2xl:h-8 m-auto"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2.5"
+                d={
+                  isDarkMode
+                    ? "M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                    : "M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                }
+              />
+            </svg>
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
--- my-landing-page/src/app/components/Header/Navigator.tsx
+++ my-landing-page/src/app/components/Header/Navigator.tsx
@@ -9,9 +9,8 @@ function Navigator() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <nav className="bg-transparent px-4 py-5 shadow-xl rounded-xl">
+    <nav className="bg-transparent px-4 py-5 shadow-2xl shadow-purple-950 rounded-2xl">
       <div className="container flex flex-wrap items-center justify-between mx-auto rounded-lg">
-
         <button
           type="button"
           className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
@@ -22,95 +21,109 @@ function Navigator() {
         </button>
 
         <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-          <Dialog.Panel
-              className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden rounded-lg"
-          >
-              <div className="flex items-center justify-between rounded-lg">
-                <button
-                  type="button"
-                  className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                  onClick={() => setMobileMenuOpen(false)}
-                >
-                  <span className="sr-only">Close menu</span>
-                  <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-              </div>
+          <Dialog.Panel className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden rounded-lg">
+            <div className="flex items-center justify-between rounded-xl">
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
 
-              <div className="mt-6 flow-root rounded-lg">
-                <div className="-my-6 divide-y divide-gray-500/10 rounded-lg">
-                  <div className="space-y-2 py-6 rounded-xl">
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg py-2 px-3 text-base font-medium leading-7 text-gray-900 hover:bg-gray-400/10"
-                    >
-                      Features
-                    </a>
+            <div className="mt-6 flow-root rounded-lg">
+              <div className="-my-6 divide-y divide-gray-500/10 rounded-lg">
+                <div className="space-y-2 py-6 rounded-xl">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg py-2 px-3 text-base font-medium leading-7 text-gray-900 hover:bg-gray-400/10"
+                  >
+                    Features
+                  </a>
 
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg py-2 px-3 text-base font-medium leading-7 text-gray-900 hover:bg-gray-400/10"
-                    >
-                      Installation
-                    </a>
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg py-2 px-3 text-base font-medium leading-7 text-gray-900 hover:bg-gray-400/10"
+                  >
+                    Installation
+                  </a>
 
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg py-2 px-3 text-base font-medium leading-7 text-gray-900 hover:bg-gray-400/10"
-                    >
-                      Usage
-                    </a>
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg py-2 px-3 text-base font-medium leading-7 text-gray-900 hover:bg-gray-400/10"
+                  >
+                    Usage
+                  </a>
 
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg py-2 px-3 text-base font-medium leading-7 text-gray-900 hover:bg-gray-400/10"
-                    >
-                      License
-                    </a>
-                  </div>
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg py-2 px-3 text-base font-medium leading-7 text-gray-900 hover:bg-gray-400/10"
+                  >
+                    License
+                  </a>
                 </div>
               </div>
-            </Dialog.Panel>
-          </Dialog>
+            </div>
+          </Dialog.Panel>
+        </Dialog>
 
-          <div className="hidden w-full md:block md:w-auto rounded-lg">
-            <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 rounded-lg">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-700 rounded-lg hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                >
-                  Features
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-700 rounded-lg hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                >
-                  Installation
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-700 rounded-lg hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                >
-                  Usage
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-700 rounded-lg hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                >
-                  License
-                </a>
-              </li>
-            </ul>
-          </div>
+        <div className="hidden w-full md:block md:w-auto shadow-2xl rounded-xl">
+          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 rounded-lg">
+            <li>
+              <a
+                href="#"
+                className="group relative inline-block overflow-hidden rounded-2xl border border-indigo-400 bg-indigo-950  px-4 py-2 text-sm font-medium text-indigo-400 hover:text-violet-600 focus:outline-none focus:ring active:bg-indigo-600 active:text-white"
+              >
+                <span className="ease absolute left-0 top-0 h-0 w-0 border-t-4 border-violet-600 transition-all duration-200 group-hover:w-full"></span>
+                <span className="ease absolute right-0 top-0 h-0 w-0 border-r-4 border-violet-600 transition-all duration-200 group-hover:h-full"></span>
+                <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-4 border-violet-600 transition-all duration-200 group-hover:w-full"></span>
+                <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-4 border-violet-600 transition-all duration-200 group-hover:h-full"></span>
+                Features
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="group relative inline-block overflow-hidden rounded-2xl border border-indigo-400 bg-indigo-950  px-4 py-2 text-sm font-medium text-indigo-400 hover:text-violet-600 focus:outline-none focus:ring active:bg-indigo-600 active:text-white"
+              >
+                <span className="ease absolute left-0 top-0 h-0 w-0 border-t-4 border-violet-600 transition-all duration-200 group-hover:w-full"></span>
+                <span className="ease absolute right-0 top-0 h-0 w-0 border-r-4 border-violet-600 transition-all duration-200 group-hover:h-full"></span>
+                <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-4 border-violet-600 transition-all duration-200 group-hover:w-full"></span>
+                <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-4 border-violet-600 transition-all duration-200 group-hover:h-full"></span>
+                Installation
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="group relative inline-block overflow-hidden rounded-2xl border border-indigo-400 bg-indigo-950  px-4 py-2 text-sm font-medium text-indigo-400 hover:text-violet-600 focus:outline-none focus:ring active:bg-indigo-600 active:text-white"
+              >
+                <span className="ease absolute left-0 top-0 h-0 w-0 border-t-4 border-violet-600 transition-all duration-200 group-hover:w-full"></span>
+                <span className="ease absolute right-0 top-0 h-0 w-0 border-r-4 border-violet-600 transition-all duration-200 group-hover:h-full"></span>
+                <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-4 border-violet-600 transition-all duration-200 group-hover:w-full"></span>
+                <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-4 border-violet-600 transition-all duration-200 group-hover:h-full"></span>
+                Usage
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="group relative inline-block overflow-hidden rounded-2xl border border-indigo-400 bg-indigo-950  px-4 py-2 text-sm font-medium text-indigo-400 hover:text-violet-600 focus:outline-none focus:ring active:bg-indigo-600 active:text-white"
+              >
+                <span className="ease absolute left-0 top-0 h-0 w-0 border-t-4 border-violet-600 transition-all duration-200 group-hover:w-full"></span>
+                <span className="ease absolute right-0 top-0 h-0 w-0 border-r-4 border-violet-600 transition-all duration-200 group-hover:h-full"></span>
+                <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-4 border-violet-600 transition-all duration-200 group-hover:w-full"></span>
+                <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-4 border-violet-600 transition-all duration-200 group-hover:h-full"></span>
+                License
+              </a>
+            </li>
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
-};
+}
 
 export default Navigator;
